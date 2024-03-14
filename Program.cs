21b6f0045e247332dde9c0809de7ee900d690d74
Charleu Limeira <sdlimeira0@gmail.com>
2024-03-14 00:25:09 -0300

    Criando a classe Aluno

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediaTrimestralOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
