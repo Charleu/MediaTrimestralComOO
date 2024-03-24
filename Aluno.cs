271a9f306b90ba6e54a7c5c41e2886c79a5733d9
Charleu Limeira <sdlimeira0@gmail.com>
2024-03-24 11:58:56 -0300

    Adicionando parâmetros ao Program e a classe Aluno

--- Aluno.cs
+++ Aluno.cs
@@ -1,10 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MediaTrimestralOO
 {
     class Aluno
     {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public Aluno(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void InformarNotas(double nota1, double nota2, double nota3)
+        {
+            string sep = new string('-', 50);
+            Console.WriteLine(sep);
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            return (Nota1 * 30 + Nota2 * 35 + Nota3 * 35) / 100;
+        }
+
+        public void ExibirResultado()
+        {
+            string sep = new string('-', 50);
+            double notaFinal = CalcularNotaFinal();
+           
+            Console.WriteLine($"Nota final de {Nome}: {notaFinal.ToString("F2")}");
+            if (notaFinal >= 60)
+            {
+                Console.WriteLine(sep);
+                Console.WriteLine("APROVADO!");
+                Console.WriteLine(sep);
+            }
+            else
+            {
+                double pontosFaltantes = 60 - notaFinal;
+                Console.WriteLine(sep);
+                Console.WriteLine($"REPROVADO! Faltam {pontosFaltantes.ToString("F2")} pontos para ser aprovado.");
+                Console.WriteLine(sep);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace MediaTrimestralOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o nome do aluno:");
+            string nome = Console.ReadLine();
+            Aluno aluno = new Aluno(nome);
+            Console.WriteLine($"Informe a nota do primeiro trimestre:");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Informe a nota do segundo trimestre:");
+            double nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Informe a nota do terceiro trimestre:");
+            double nota3 = double.Parse(Console.ReadLine());
+            aluno.InformarNotas(nota1, nota2, nota3);
+            aluno.ExibirResultado();
         }
     }
 }
